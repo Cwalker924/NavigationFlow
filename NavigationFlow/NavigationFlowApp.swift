61fb85d84c65cb7cdbf0d9ae4fef4b2d04ab0ae5
codyBo <cwalker924@gmail.com>
2023-06-17 00:25:12 -0700

    Initial Commit

---
+++ NavigationFlow/NavigationFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationFlowApp.swift
+//  NavigationFlow
+//
+//  Created by Colton Walker on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
